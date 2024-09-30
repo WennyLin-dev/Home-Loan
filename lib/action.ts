5414d8b096bd5489e646a931bbe8d89a40ce1b58
WennyLin-dev <wenny518999@gmail.com>
2024-09-30 14:57:59 +1300

    f

--- lib/action.ts
+++ lib/action.ts
@@ -1,25 +1,28 @@
-// import bcrypt from "bcrypt";
-// import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
+import { prisma } from "@/lib/prisma";
 
-const login = async () => {
-    try {
-      // const user = await prisma
-      
-      // ({ username: credentials.username });
-  
-      // if (!user || !user.isAdmin) throw new Error("Wrong credentials!");
-  
-      // const isPasswordCorrect = await bcrypt.compare(
-      //   credentials.password,
-      //   user.password
-      // );
-  
-      // if (!isPasswordCorrect) throw new Error("Wrong credentials!");
-  
-      return true;
-    } catch (err) {
-      console.log(err);
-      throw new Error("Failed to login!");
-    }
-  };
-  export {login}
+const login = async (credentials: {
+  username: string,
+  password: string
+}) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: credentials.username
+      }
+    })
+
+    if (!user) throw new Error("Wrong credentials!");
+
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    );
+    if (!isPasswordCorrect) throw new Error("Wrong credentials!");
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to login!");
+  }
+};
+export { login }
